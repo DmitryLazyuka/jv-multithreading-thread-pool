6d4ad2966ba314542d6bbd047a265ba3f8d0c000
dmytro.lazyuka <dmytro.lazyuka@nixs.com>
2026-02-10 14:11:25 +0100

    Thread Pool Task remove try-with-resources

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,17 +13,18 @@ public class Main {
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
 
-        try (ExecutorService executorService = Executors.newFixedThreadPool(5)) {
-            for (int i = 0; i < 20; i++) {
-                futures.add(executorService.submit(new MyThread()));
-            }
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
         }
+
 
         for (Future<String> future : futures) {
             try {
                 System.out.println(future.get());
             } catch (InterruptedException | ExecutionException e) {
-                throw  new RuntimeException(e);
+                throw new RuntimeException(e);
             }
         }
     }
