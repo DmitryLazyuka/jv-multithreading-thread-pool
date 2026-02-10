6d6aa70025031717adbc7204f1a7f2436500201a
dmytro.lazyuka <dmytro.lazyuka@nixs.com>
2026-02-10 14:09:14 +0100

    Thread Pool Task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +11,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        try (ExecutorService executorService = Executors.newFixedThreadPool(5)) {
+            for (int i = 0; i < 20; i++) {
+                futures.add(executorService.submit(new MyThread()));
+            }
+        }
+
+        for (Future<String> future : futures) {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw  new RuntimeException(e);
+            }
+        }
     }
 }
